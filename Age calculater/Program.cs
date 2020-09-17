a524022fe0ca4e06af0099c5d6ea7c8a9c36da80
User131124 <46863300+User131124@users.noreply.github.com>
2020-09-17 19:59:26 +0500

    Add files via upload

---
+++ Age calculater/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace AgeCalculater
+{
+    class Program
+    {
+        public static void Main()
+        {
+            DateTime thisDay = DateTime.Today;
+            int dayofbearth;
+            int monthofbearth;
+            int yearofbearth;
+            int years;
+            int[] months = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int day = Convert.ToInt32(thisDay.ToString("dd"));
+            int month = Convert.ToInt32(thisDay.ToString("MM"));
+            int year = Convert.ToInt32(thisDay.ToString("yyyy"));
+            Console.WriteLine("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            do
+            {
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите день своего рождения");
+                        dayofbearth = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите месяц своего рождения");
+                        monthofbearth = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите год своего рождения");
+                        yearofbearth = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Ошибка ввода");
+                    }
+                } while (true);
+
+                if (yearofbearth % 4 == 0 && monthofbearth == 2)
+                {
+                    ++months[1];
+                }
+                if (monthofbearth < 12 && monthofbearth > 0 && dayofbearth > 0 && dayofbearth < months[monthofbearth] + 1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введена неверная дата");
+                }
+            } while (true);
+            years = year - yearofbearth;
+            if (month < monthofbearth)
+            {
+                Console.WriteLine("Привет, {0}. Ваш возраст равен {1} лет. Приятно познакомиться.", name, --years);
+            }
+            if (month >= monthofbearth)
+            {
+                if((month == monthofbearth && dayofbearth >= day) || month > monthofbearth)
+                {
+                    Console.WriteLine("Привет, {0}. Ваш возраст равен {1} лет. Приятно познакомиться.", name, years);
+                }
+                else
+                {
+                    Console.WriteLine("Привет, {0}. Ваш возраст равен {1} лет. Приятно познакомиться.", name, --years);
+                }
+            }
+            
+        }
+    }
+}
